bac64f338c91bad73ca3d59866c85d30bf089328
Aurore Trunelle <aurore.trunelle@gmail.com>
2019-03-28 13:15:04 +0000

    chore: remove non needed code

--- vuejs/vue.config.js
+++ vuejs/vue.config.js
@@ -1,5 +1,3 @@
-const cookie = 'PLAY_SESSION=8ab8348690744852b11fb9f017d0785ac2310128-username=smartmatchtest';
-
 module.exports = {
   runtimeCompiler: true,
   devServer: {
@@ -8,9 +6,9 @@ module.exports = {
         target: 'https://api.edamam.com/',
         changeOrigin: true,
         pathRewrite: {
-          '^/api': ''
-        }
-      }
+          '^/api': '',
+        },
+      },
     },
   },
 };
